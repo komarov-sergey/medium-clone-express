0dba59f26c6d3194a1c7e59de3fb656ab7bdbd22
Sergey <komarovs33@mail.ru>
2020-07-12 00:14:16 +0300

    finish

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -36,17 +36,74 @@ router.param("comment", function (req, res, next, id) {
     .catch(next);
 });
 
+router.get("/", auth.optional, function (req, res, next) {
+  const query = {};
+  const limit = 20;
+  const offset = 0;
+
+  if (typeof req.query.limit !== "undefined") {
+    limit = req.query.limit;
+  }
+
+  if (typeof req.query.offset !== "undefined") {
+    offset = req.query.offset;
+  }
+
+  if (typeof req.query.tag !== "undefined") {
+    query.tagList = { $in: [req.query.tag] };
+  }
+
+  Promise.all([
+    req.query.author ? User.findOne({ username: req.query.author }) : null,
+    req.query.favorited
+      ? User.findOne({ username: req.query.favorited })
+      : null,
+  ])
+    .then(function (results) {
+      const author = results[0];
+      const favoriter = results[1];
+
+      if (author) {
+        query.author = author._id;
+      }
+
+      if (favoriter) {
+        query._id = { $in: favoriter.favorites };
+      } else if (req.query.favorited) {
+        query._id = { $in: [] };
+      }
+
+      return Promise.all([
+        Article.find(query)
+          .limit(Number(limit))
+          .skip(NUmber(offset))
+          .sort({ createdAt: "desc" })
+          .populate("author")
+          .exec(),
+        Article.count(query).exac(),
+        req.payload ? User.findById(req.payload.id) : null,
+      ]).then(function (results) {
+        const articles = result[0];
+        const articlesCount = result[1];
+        const user = results[2];
+
+        return res.json({
+          articles: articles.map((article) => article.toJSONFor(user)),
+          articlesCount,
+        });
+      });
+    })
+    .catch(next);
+});
+
 router.post("/", auth.required, function (req, res, next) {
   User.findById(req.payload.id)
     .then(function (user) {
       if (!user) {
         return res.sendStatus(401);
       }
-
       const article = new Article(req.body.article);
-
       article.author = user;
-
       return article.save().then(function () {
         console.log(article.author);
         return res.json({ article: article.toJSONFor(user) });
@@ -211,6 +268,44 @@ router.delete("/:article/comments/:comment", auth.required, function (
   } else {
     res.sendStatus(403);
   }
+});
+
+router.get("/feed", auth.required, function (req, res, next) {
+  const limit = 20;
+  const offset = 0;
+
+  if (typeof req.query.limit !== "undefined") {
+    limit = req.query.limit;
+  }
+
+  if (typeof req.query.offset !== "undefined") {
+    offset = req.query.offset;
+  }
+
+  User.findById(req.payload.id).then(function (user) {
+    if (!user) return res.sendStatus(401);
+
+    Promise.all([
+      Article.find({ author: { $in: user.following } })
+        .limit(NUmber(limit))
+        .skip(Number(offset))
+        .populate("author")
+        .exec(),
+      Article.count({ author: { $in: user.following } }),
+    ])
+      .then((results) => {
+        const articles = results[0];
+        const articlesCount = results[1];
+
+        return res.json({
+          articles: articles.map(function (article) {
+            return article.toJSONFor(user);
+          }),
+          articlesCount: articlesCount,
+        });
+      })
+      .catch(next);
+  });
 });
 
 module.exports = router;
